caaf66bcbf69ae74d41c20bb97933e39740f57c7
zyb8023 <fengying11734@126.com>
2019-01-21 17:36:57 +0800

    vue基础版本

--- src/filters/formatTime.js
+++ src/filters/formatTime.js
@@ -3,7 +3,7 @@ export const formatTime = (time) => {
     let d = new Date();
     d.setTime(time);
     let year = d.getFullYear();
-    let month = d.getMonth();
+    let month = d.getMonth()+1;
     let day = d.getDate();
     return ` ${createZero(year)} 年 ${createZero(month)} 月 ${createZero(day)} 日`
 }
